cbd7269f1c7fab1ded328d03b5a580432da6530c
Nonglak Saeueng <mam.nonglaks@gmail.com>
2021-06-23 16:48:50 -0700

    update db.js to connect with local db

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,13 @@
 const Sequelize = require("sequelize");
+require('dotenv').config();
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
+// const db = new Sequelize(process.env.DATABASE_URL || "postgres://postgres:localhost:5432/messenger", {
+//   logging: false
+// });
+
+const db = new Sequelize(process.env.DATABASE_URL ||'messenger', 'emmi', process.env.PGPASSWORD, {
+  host: 'localhost',
+  dialect:'postgres',
   logging: false
 });
 
