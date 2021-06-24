60ba586550d4665bb4d135de78caad32c8883d56
Nonglak Saeueng <mam.nonglaks@gmail.com>
2021-06-24 11:00:20 -0700

    delete comment and add expiration to cookie

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,6 @@
 const Sequelize = require("sequelize");
 require('dotenv').config();
 
-// const db = new Sequelize(process.env.DATABASE_URL || "postgres://postgres:localhost:5432/messenger", {
-//   logging: false
-// });
-
 const db = new Sequelize(process.env.DATABASE_URL ||'messenger', 'emmi', process.env.PGPASSWORD, {
   host: 'localhost',
   dialect:'postgres',
--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -27,7 +27,7 @@ router.post("/register", async (req, res, next) => {
       { expiresIn: 86400 }
     );
 
-    res.cookie("token", token, {httpOnly: true});
+    res.cookie("token", token, {httpOnly: true, maxAge: 86400});
 
     res.json({
       ...user.dataValues,
@@ -67,7 +67,7 @@ router.post("/login", async (req, res, next) => {
         process.env.SESSION_SECRET,
         { expiresIn: 86400 }
       );
-      res.cookie("token", token, {httpOnly: true});
+      res.cookie("token", token, {httpOnly: true, maxAge: 86400});
       res.json({
         ...user.dataValues,
         token,
