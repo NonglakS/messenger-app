f6530df517be3ef3714965f661bcc77d5bbd43d8
Nonglak Saeueng <mam.nonglaks@gmail.com>
2021-06-28 21:50:39 -0700

    add icon indicate a message is seen

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
@@ -9,13 +9,39 @@ const Messages = (props) => {
     return x.id - y.id;
   });
 
+  const [lastReadIndex, setLastReadIndex] = useState(-1);
+
+  const lastSeenMessage = () => {
+    let lastIndex = -1;
+    for (let i = 0; i < messages.length; i++) {
+      if (
+        messages[i].isRead &&
+        messages[i].id > lastIndex &&
+        messages[i].senderId === userId
+      ) {
+        lastIndex = messages[i].id;
+      }
+      setLastReadIndex(lastIndex);
+    }
+  };
+
+  useEffect(() => {
+    lastSeenMessage();
+  }, [messages]);
+
   return (
     <Box>
       {messageList.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
+          <SenderBubble
+            key={message.id}
+            text={message.text}
+            time={time}
+            otherUser={otherUser}
+            seenIndex={message.id === lastReadIndex}
+          />
         ) : (
           <OtherUserBubble
             key={message.id}
